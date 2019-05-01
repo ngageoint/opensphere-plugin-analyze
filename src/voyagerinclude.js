02a58336c27d314280339fd0eff0106869c5660c
William Wall <wwall@caci.com>
2019-05-01 09:19:38 -0600

    fix(voyager): fix voyager plugin addition

--- src/voyagerinclude.js
+++ src/voyagerinclude.js
@@ -2,5 +2,5 @@ goog.provide('voyager');
 goog.require('plugin.voyager.VoyagerPlugin');
 
 (function() {
-  os.plugin.PluginManager.getInstance().addPlugin(plugin.voyager.VoyagerPlugin.getInstance());
+  os.plugin.PluginManager.getInstance().addPlugin(new plugin.voyager.VoyagerPlugin());
 })();
