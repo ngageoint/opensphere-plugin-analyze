efcd32423ae33e2db1584ff2379226d6376bd907
Kevin Schmidt <kschmidt@caci.com>
2021-11-15 15:19:24 -0700

    refactor(places): add show in analyze to base places options

--- src/mist/mixin/placesmanager.js
+++ src/mist/mixin/placesmanager.js
@@ -1,29 +1,7 @@
 goog.declareModuleId('mist.mixin.places');
 
-import PlacesManager from 'opensphere/src/plugin/places/placesmanager.js';
+import {BASE_OPTIONS} from 'opensphere/src/plugin/places/placesmanager.js';
 import {ANALYZE_CONFIG_KEY} from './vectorsource.js';
 
-/**
- * mixin to make saved places default to not show in analyze
- * @suppress {accessControls}
- */
-(function() {
-  const old = PlacesManager.prototype.getOptions;
-
-  /**
-   * options used to create the places layer
-   * @return {Object<string, *>} the layer options
-   * @protected
-   * @override
-   */
-  PlacesManager.prototype.getOptions = function() {
-    const options = old.call(this);
-
-    // ensure we default to false
-    if (!(ANALYZE_CONFIG_KEY in options)) {
-      options[ANALYZE_CONFIG_KEY] = false;
-    }
-
-    return options;
-  };
-})();
+// Default "Show in Analyze" to false.
+BASE_OPTIONS[ANALYZE_CONFIG_KEY] = false;
