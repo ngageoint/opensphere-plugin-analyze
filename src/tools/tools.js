402c79cabc0c6531cbaa6761854bfac7da4fdfa5
Brandon Cleveland <bcleveland1022@gmail.com>
2022-04-27 10:13:23 -0600

    fix: analyze tools now work in prod build

--- src/tools/tools.js
+++ src/tools/tools.js
@@ -1,7 +1,12 @@
 goog.declareModuleId('tools');
-import {APP_ROOT} from 'opensphere/src/os/os.js';
+import {ROOT as APP_ROOT} from 'opensphere/src/os/os.js';
+
+let appRoot = APP_ROOT;
+if (appRoot == '../opensphere/') {
+  appRoot = '../opensphere-plugin-analyze/';
+}
 
 /**
  * @define {string} The base path to this project.
  */
-export const ROOT = APP_ROOT;
+export const ROOT = appRoot;
